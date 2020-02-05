3a3b1da6bd38afe69926f249f11a5e1e3ea920eb
Eric Matthes <eric@Erics-MacBook-Pro.local>
2020-02-05 13:55:34 -0900

    Added notes about next stage of work.

--- analyze_current_data.py
+++ analyze_current_data.py
@@ -18,3 +18,14 @@ readings = a_utils.process_xml_data(current_data)
 recent_readings = a_utils.get_recent_readings(readings, 48)
 critical_points = a_utils.get_critical_points(recent_readings)
 plot_utils.plot_current_data_html(recent_readings)
+
+# Make a data file of all hx IRReading objects, to make the rest of the
+#   work easier. Probably needs to be two sets, with consistent
+#   reading intervals.
+# Make a data file of 1 week of readings before moment of Kramer Ave slide.
+# Pull from this file to get kramer_readings.
+# Get recent_kramer_readings.
+# Plot recent_kramer_readings with lower alpha values.
+
+# Repeat for Medvejie 9/2019 slide.
+# Put this on a heroku app?
