a5ea9b75fde3028051fb149e00a325f076b60cce
Eric Matthes <eric@Erics-MacBook-Pro.local>
2020-08-03 07:24:00 -0800

    Refactored testing code to support more tests.

--- tests/test_process_hx_data.py
+++ tests/test_process_hx_data.py
@@ -8,32 +8,44 @@ $ python -m pytest
 
 from os import listdir, path
 from pathlib import Path
+import os, shutil
 
 import pytest
 
 from sitka_irg_analysis import process_hx_data as phd
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def run_process_hx_data():
+    # Clear any previous output.
+    shutil.rmtree('tests/current_ir_plots')
+    shutil.rmtree('tests/other_output')
+    os.mkdir('tests/current_ir_plots')
+    os.mkdir('tests/other_output')
+
     phd.process_hx_data(root_output_directory='tests/')
+    
+
+def get_current_ir_plots_files():
+    current_ir_plots_file_path = 'tests/current_ir_plots'
+    return [f for f in listdir(current_ir_plots_file_path)
+            if path.isfile(path.join(current_ir_plots_file_path, f))]
+
+def get_png_output_files():
+    return [f for f in get_current_ir_plots_files()
+            if Path(f).suffix=='.png']
+
+def get_reference_files():
+    ref_file_path = 'tests/reference_files'
+    return [f for f in listdir(ref_file_path)
+            if path.isfile(path.join(ref_file_path, f))]
+
+def get_png_reference_files():
+    return [f for f in get_reference_files() if Path(f).suffix=='.png']
+
 
 def test_png_output_files_exist(run_process_hx_data):
-    png_file_path = 'tests/current_ir_plots'
-    current_ir_plots_files = [f for f in listdir(png_file_path)
-            if path.isfile(path.join(png_file_path, f))]
-
-    png_output_files = [f for f in current_ir_plots_files
-            if Path(f).suffix=='.png']
-
-    ref_file_path = 'tests/reference_files'
-    reference_files = [f for f in listdir(png_file_path)
-            if path.isfile(path.join(png_file_path, f))]
-    png_ref_files = [f for f in reference_files
-            if Path(f).suffix=='.png']
-
     # Assert output png file names match reference png file names.
+    png_output_files = get_png_output_files()
+    png_ref_files = get_png_reference_files()
     assert(set(png_output_files) == set(png_ref_files))
-
-
-
