9b44b6a14fa204af5f47cd2c04145da92d8b0fe3
Eric Matthes <eric@Erics-MacBook-Pro.local>
2020-08-25 09:59:21 -0800

    Stub test passes.

---
+++ tests/test_process_hx_data.py
@@ -0,0 +1,12 @@
+"""Tests for process_hx_data.py.
+
+Focuses on verifying png and pkl output.
+
+Run this from project root directory:
+$ python -m pytest
+"""
+
+from sitka_irg_analysis import process_hx_data as phd
+
+def test_png_output():
+    pass
