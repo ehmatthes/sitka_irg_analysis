2c3b5b4be284a9b9f16c66c4668c07896e0bcee6
Eric Matthes <eric@Erics-MacBook-Pro.local>
2020-08-30 11:20:17 -0800

    Can use cached data.

--- process_hx_data.py
+++ process_hx_data.py
@@ -32,7 +32,10 @@ We want to be able to use the output to answer the following questions. In
       positive)
 """
 
-import argparse, sys
+import argparse, sys, pickle
+
+from os import listdir, path
+from pathlib import Path
 
 import plot_heights as ph
 from slide_event import SlideEvent
@@ -47,6 +50,9 @@ parser.add_argument('--no-interactive-plots',
     action='store_true')
 parser.add_argument('--no-static-plots',
     help="Do not generate static plots.",
+    action='store_true')
+parser.add_argument('--use-cached-data',
+    help="Use pickled data; don't parse raw data files.",
     action='store_true')
 args = parser.parse_args()
 
@@ -72,45 +78,60 @@ def process_hx_data(root_output_directory=''):
     slides_file = 'known_slides/known_slides.json'
     known_slides = SlideEvent.load_slides(slides_file)
 
-    # DEV: Should probably walk the ir_data_clean directory, instead of making
-    #      this list manually.
-    data_files = [
-        'ir_data_clean/irva_utc_072014-022016_hx_format.txt',
-        'ir_data_clean/irva_akdt_022016-102019_arch_format.txt',
-    ]
+    reading_sets = []
 
-    for data_file in data_files:
-        readings = a_utils.get_readings_from_data_file(data_file)
-        reading_sets = a_utils.get_reading_sets(
-                readings, known_slides, stats)
+    if not args.use_cached_data:
+        print("Parsing raw data files...")
+        data_files = [
+            'ir_data_clean/irva_utc_072014-022016_hx_format.txt',
+            'ir_data_clean/irva_akdt_022016-102019_arch_format.txt',
+        ]
 
-        # Pickle reading sets for faster analysis and plotting later,
-        #   and for use by other programs.
+        for data_file in data_files:
+            readings = a_utils.get_readings_from_data_file(data_file)
+            reading_sets += a_utils.get_reading_sets(readings, known_slides, stats)
+
+        print("Pickling reading sets...")
         for reading_set in reading_sets:
-            print("Pickling reading sets...")
+            # Pickle reading sets for faster analysis and plotting later,
+            #   and for use by other programs.
             a_utils.pickle_reading_set(reading_set, root_output_directory)
 
-        if not args.no_interactive_plots:
-            for reading_set in reading_sets:
-                print("Generating interactive plots...")
-                critical_points = a_utils.get_critical_points(reading_set)
-                ph.plot_data(
-                    reading_set,
-                    known_slides=known_slides,
-                    critical_points=critical_points,
-                    root_output_directory=root_output_directory)
+    elif args.use_cached_data:
+        print("Reading data from pickled files...")
+        pkl_file_path = 'other_output/'
+        pkl_files = [f for f in listdir(pkl_file_path)
+                if path.isfile(path.join(pkl_file_path, f))
+                and Path(f).suffix=='.pkl']
 
-        if not args.no_static_plots:
-            for reading_set in reading_sets:
-                print("Generating static plots...")
-                critical_points = a_utils.get_critical_points(reading_set)
-                ph.plot_data_static(
-                    reading_set,
-                    known_slides=known_slides,
-                    critical_points=critical_points,
-                    root_output_directory=root_output_directory)
+        for pkl_file in pkl_files:
+            filename = f"{pkl_file_path}{pkl_file}"
+            with open(filename, 'rb') as f:
+                reading_set = pickle.load(f)
+                reading_sets.append(reading_set)
 
-    a_utils.summarize_results(known_slides, stats)
+    if not args.no_interactive_plots:
+        for reading_set in reading_sets:
+            print("Generating interactive plots...")
+            critical_points = a_utils.get_critical_points(reading_set)
+            ph.plot_data(
+                reading_set,
+                known_slides=known_slides,
+                critical_points=critical_points,
+                root_output_directory=root_output_directory)
+
+    if not args.no_static_plots:
+        for reading_set in reading_sets:
+            print("Generating static plots...")
+            critical_points = a_utils.get_critical_points(reading_set)
+            ph.plot_data_static(
+                reading_set,
+                known_slides=known_slides,
+                critical_points=critical_points,
+                root_output_directory=root_output_directory)
+
+    if not args.use_cached_data:
+        a_utils.summarize_results(known_slides, stats)
 
 
 if __name__ == '__main__':
